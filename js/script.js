fd51c087dd8801b29395e7ac80810b069bf902bf
devprincess <princess.solomon@eng.uniben.edu>
2023-08-24 21:09:44 +0100

    added search functionalities and all images

--- js/script.js
+++ js/script.js
@@ -1,17 +1,10 @@
-//create the food items with indexes
-//create the cart object
-//updating the cart object
-//Capture modal opening and open manually so we can remove the cart items from storage...
-
+document.querySelector('a.btn.btn-primary.mt-3').style.opacity = 0.5;
+var searchString = "", searchCategory = 0, categoryRef = {0: 'all', 1: 'Nigerian', 2: 'Italian', 3: 'Chinese', 4: 'American', 5: 'Canadian'}, sliderMin = 1000, sliderMax = 10000;
 var cartObj = {};
-let items = [
-    ['rice 2.jpg', 'Sweet Fried Rice', 'Nigerian', '500'],
-    ['okro soup.jpg', 'Okra Soup','Nigerian', '600']
-]
 let foodItems = document.querySelector('.food-items');
 let cartItems = document.querySelector('.cart-items');
 items.forEach((item, index) => {
-    let pic = item[0], name = item[1], cate = item[2], pric = item[3];
+    let pic = item[2], name = item[1], cate = item[0], pric = item[3];
     var foodItem = document.createElement('div');
     foodItem.className = 'col-sm-6 col-md-4 col-lg-3 p-3';
     foodItem.innerHTML = `<div class="card text-center shadow-sm">
@@ -35,9 +28,9 @@ function addItem(index){
     let cartItem = document.createElement('li');
     cartItem.className = "nav-item";
     cartItem.innerHTML = `<div class="input-group mb-3">
-                            <span class="input-group-text">${cartNum}.</span>
-                            <span class="input-group-text">${foodName}</span>
-                            <span class="input-group-text cart-list-price">&#8358;<span>${foodPric}</span></span>
+                            <span class="input-group-text __no-select">${cartNum}.</span>
+                            <span class="input-group-text __no-select">${foodName}</span>
+                            <span class="input-group-text cart-list-price __no-select">&#8358;<span>${foodPric}</span></span>
                             <input type="number" class="form-control" onchange="updateCart()" value="${foodQty}">
                         </div>`;
     cartItems.appendChild(cartItem);
@@ -45,11 +38,10 @@ function addItem(index){
     foodItems.querySelectorAll('div.p-3')[index].querySelector('a').innerHTML='In Basket';
     foodItems.querySelectorAll('div.p-3')[index].querySelector('a').classList.add('stactic');
     updateCart();
-    //cart object is going to look this way...
-    //foodname,price,qty
 }
 
 function updateCart(){
+    let checkoutBtn = document.querySelector('a.btn.btn-primary.mt-3');
     let allCartItems = cartItems.querySelectorAll('li.nav-item');
     let totalPrice = 0;
     allCartItems.forEach((cartItem, index) => {
@@ -58,11 +50,18 @@ function updateCart(){
             totalPrice += (qty * price);
         }
     });
-    document.querySelector('.total-price-span').innerHTML = totalPrice;
-    let cartCount = cartItems.querySelectorAll('li').length-1;
-    document.querySelectorAll('.cart-label').forEach((label)=>{
-        label.innerHTML = cartCount;
-    });
+    if(totalPrice == 0){
+        checkoutBtn.setAttribute('onclick', null);
+        checkoutBtn.style.opacity = 0.5;
+    }else{
+        checkoutBtn.setAttribute('onclick', 'openSuccess();');
+        checkoutBtn.style.opacity = 1;
+        document.querySelector('.total-price-span').innerHTML = totalPrice;
+        let cartCount = cartItems.querySelectorAll('li').length-1;
+        document.querySelectorAll('.cart-label').forEach((label)=>{
+            label.innerHTML = cartCount;
+        });
+    }
 }
 
 function openSuccess(){
@@ -76,3 +75,103 @@ function openSuccess(){
     ogbolor.hide();
     modal.show();
 }
+
+function doCollate(){
+    let strMatches = 0;
+    document.querySelectorAll('.search-matches')[0].classList.add('unsee');
+    document.querySelectorAll('.search-matches')[1].classList.add('unsee');
+    let searchItems = document.querySelectorAll('.row.food-items > div');
+    searchItems.forEach((searchItem) => {
+        let isSearch = sortBySearch(searchString, searchItem);
+        let isCategory = sortByCategory(searchCategory, searchItem);
+        let isInRange = sortByPrice(sliderMin, sliderMax, searchItem);
+        if(isSearch && isCategory && isInRange){
+            searchItem.classList.remove('unsee');
+            strMatches++;  
+        }else searchItem.classList.add('unsee'); false;
+        //searchItem.classList.remove('unsee'); true //strMatches++;  searchItem.classList.remove('unsee');
+        //searchItem.classList.add('unsee'); false;
+   });
+    if(searchString != "" || sliderMin != 1000 || sliderMax != 10000){
+        let pluralize = (strMatches == 1)?'':'es';
+        document.querySelectorAll('.search-matches')[0].classList.remove('unsee');
+        document.querySelectorAll('.search-matches')[1].classList.remove('unsee');
+        document.querySelectorAll('.search-matches')[0].innerHTML = strMatches+' Match'+pluralize+' Found';
+        document.querySelectorAll('.search-matches')[1].innerHTML = strMatches+' Match'+pluralize+' Found';
+    }
+}
+
+function doCategory(qCategory){
+    searchCategory = qCategory;
+    document.querySelectorAll('.navbar-nav > li.nav-item').forEach(liItem => liItem.classList.remove('active'));
+    document.querySelectorAll('.navbar-nav > li.nav-item')[qCategory+3].classList.add('active');
+    doCollate();
+}
+
+function doSearch(qString){
+    searchString = qString;
+    document.querySelectorAll('input[type="search"]').forEach(searchInput => searchInput.value = qString);
+    doCollate();
+}
+
+function sortBySearch(qString, searchItem){
+    if(qString == "") return true;
+    else {
+        let textItem = searchItem.querySelector('h5.card-title').innerHTML;
+        if(textItem.toLowerCase().search(qString) == -1) return false;
+        else return true;
+    }
+}
+
+function sortByCategory(qCategory, searchItem){
+    if(qCategory == 0) return true;
+    else{
+        let textItem = searchItem.querySelector('.span-category').innerHTML;
+        if(textItem == categoryRef[qCategory]) return true;
+        else return false;
+    }
+}
+
+function sortByPrice(lowerLim, upperLim, searchItem){ 
+    let textItem = searchItem.querySelector('.btn-price > span').innerHTML; 
+    if(Number(textItem) >= lowerLim && Number(textItem) <= upperLim) return true;
+    else return false;
+}
+
+var slider = document.getElementById('slider');
+noUiSlider.create(slider, {
+    start: [1000, 10000],
+    connect: true,
+    step: 500,
+    margin: 500,
+    behaviour: 'tap-drag',
+    tooltips: true,
+    format: wNumb({
+        decimals: 0,
+        prefix: '&#8358;'
+    }),
+    range: {
+        'min': 1000,
+        'max': 10000
+    },
+});
+
+slider.noUiSlider.on('update', function (values, handle) {
+    sliderMin = values[0].replace('&#8358;', '');
+    sliderMax = values[1].replace('&#8358;', '');
+    doCollate();
+});
+
+//============================
+//YOUR TASKSKSSKSKSKSSKSK
+//=============================
+/* Optimize the website... 
+    1. add functionalities like deleting the whole cart, 
+    2. deleting a particular item from cart (You don't expect me to buy everything I touch)
+    3. Add a new container to the thank you page (success page), so that when I click on check out, it will ask for payment method, next take my payment details like card number blahblahblah, before showing the success screen.
+    You can do all of these inside that same modal, just hide and show the particular content using javascript when the user presses buttons, so it is easy
+    Thank you.
+*/
+//============================
+//YOUR TASKSKSSKSKSKSSKSK
+//=============================
